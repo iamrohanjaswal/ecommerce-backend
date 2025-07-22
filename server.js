9526385760eca80217dd8468c0b296752cf90efd
iamrohanjaswal <rohanjaswal45@gmail.com>
2025-07-23 00:28:26 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,24 @@
+const express =require("express");
+const cors =require("cors");
+const dotenv =require("dotenv");
+const connectDB= require("./config/db");
+
+const app=express();
+app.use(express.json());
+app.use(cors());
+
+dotenv.config();
+
+
+const PORT=process.env.PORT || 3000;
+
+//connect to mongoDB
+connectDB();
+
+app.get("/",(req,res)=>{
+    res.send("WELCOME");
+});
+
+app.listen(PORT,()=>{
+    console.log(`Server is running on http://localhost:${PORT}`);
+})
